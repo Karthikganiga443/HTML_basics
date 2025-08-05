ff1ed46374182e0618481573d19ba6ada2ad0f97
Karthikganiga443 <karthikganiga443@gmail.com>
2025-08-05 17:35:41 +0530

    done

--- Hotel_payment/hotel_payment.js
+++ Hotel_payment/hotel_payment.js
@@ -12,22 +12,24 @@ function totalconfirm() {
     var y = 65;
     const { jsPDF } = window.jspdf;
     pdf = new jsPDF();
-
     pdf.text("The Global Plate Restaurant", 70, 10);
     pdf.text("Thank you for visiting!!!", 80, 17);
     pdf.text("Malleshwaram, Bangalore", 75, 24);
-    pdf.text("--------------------------------------------------------------------------------------", 20, 31);
+    const now= new Date();
+    const pad=n=>String(n).padStart(2,"0")//adding 0's if only one number is present
+    pdf.text(`${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())} | ${pad(now.getDate())}/${pad(now.getMonth()+1)}/${pad(now.getFullYear())}`,128,29);
+    pdf.text("--------------------------------------------------------------------------------------", 20, 32);
     if (!customerName.value){
         alert("please Enter Your Name!!!");
         return;
     }
-    pdf.text(`Name : ${customerName.value}`,20,38);
+    pdf.text(`Name : ${customerName.value}`,20,39);
     pdf.text("|",100,38);
     if (!tableNumber.value){
         alert("Please Select the Table!!!");
         return;
     }
-    pdf.text(`Table number : ${tableNumber.value}`,130,38)
+    pdf.text(`Table number : ${tableNumber.value}`,130,39)
     pdf.text("--------------------------------------------------------------------------------------", 20, 45);
     pdf.text("Dishes", 20, 51);
     pdf.text("QTY", 150, 51);
@@ -46,7 +48,7 @@ function totalconfirm() {
 
             pdf.text(`${itemName}`, 20, y);
             pdf.text(`${qty}`, 152, y);
-            pdf.text(`${itemTotal}`, 167, y);
+            pdf.text(`${itemTotal}`, 176, y,{align:`right`});//end at x=176
             y += 7;
         }
     }
@@ -57,12 +59,16 @@ function totalconfirm() {
         return;
     }
 
-    pdf.text("---------------------------------------------------------------------------------------", 20, y);
+    pdf.text("--------------------------------------------------------------------------------------", 20, y);
     y += 7;
+    totalprice.innerText = `\u20B9${total}/-`;
     pdf.text("Sub Total", 20, y);
-    pdf.text(`${total} rps`, 167, y);
-
-    totalprice.innerHTML = `\u20B9${total}/-`;
+    pdf.text(`INR ${total}`, 176, y,{align:`right`});
+    y+=7;
+    pdf.text("--------------------------------------------------------------------------------------", 20, y);
+    y+=1;
+    pdf.text("--------------------------------------------------------------------------------------", 20, y);
+    
     price.showModal(); // Show payment dialog
 }
 
